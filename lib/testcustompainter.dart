cbf1a5e44c30ada87e0353cf1d2183e12ab24c95
Tran Minh Hai <hai@bio-rithm.com>
2021-03-01 08:09:44 +0700

    custompaint and custompainter with size

--- lib/testcustompainter.dart
+++ lib/testcustompainter.dart
@@ -9,30 +9,52 @@ class CustomPainterApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("CustomPainter"),
         ),
-        body: Container(
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 80),
-          child: LayoutBuilder(
-              builder: (_, contraints) => Container(
-                    width: contraints.widthConstraints().maxWidth,
-                    height: contraints.heightConstraints().maxHeight,
-                    color: Colors.yellow,
-                    child: CustomPaint(
-                      painter: FaceOutlinePainter(),
-                    ),
-                  )),
+        body: Column(
+          children: [
+            Container(
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              height: 300,
+              color: Colors.blue,
+            ),
+            CustomPaint(
+                painter: FaceOutlinePainter(),
+                child: Container(
+                  color: Colors.green.withOpacity(0.5),
+                  // width: 15,
+                  height: 300,
+                  // child: Text("OK"),
+                )),
+          ],
         ),
       ),
     );
   }
 }
 
+// Size CustomPaint
+// Container(
+//           color: Colors.white,
+//           padding: EdgeInsets.symmetric(horizontal: 40, vertical: 80),
+//           child: LayoutBuilder(
+//               builder: (_, contraints) => Container(
+//                     width: contraints.widthConstraints().maxWidth,
+//                     height: contraints.heightConstraints().maxHeight,
+//                     color: Colors.yellow,
+//                     child: CustomPaint(
+//                       painter: FaceOutlinePainter(),
+//                     ),
+//                   )),
+//         )
+
 class FaceOutlinePainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     // TODO: Draw here
     final paint = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 4.0
+      ..strokeWidth = 2.0
       ..color = Colors.indigo;
 
     //

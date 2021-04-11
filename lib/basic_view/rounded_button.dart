187c7d17f47f2fcfabd719278dcd749933444b53
Tran Minh Hai <hai@bio-rithm.com>
2021-04-11 15:38:54 +0700

    email and password text filed with boxdecoration

--- lib/basic_view/rounded_button.dart
+++ lib/basic_view/rounded_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider/basic_view/welcome_login.dart';
 
 class BasicViewApp extends StatelessWidget {
   @override
@@ -19,12 +20,64 @@ class BasicView extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+        PassFieldContainer(),
+        TextFieldContainer(),  
         RoundedButtonElevated(),
         SizedBox(height: 20,),
         RoundedButton(),
         SizedBox(height: 20,),
         RoundedButtonClip()
       ],),
+      ),
+    );
+  }
+}
+
+
+
+class PassFieldContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: kPrimaryLightColor,
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock, color: kPrimaryColor,),
+          suffixIcon: Icon(Icons.visibility, color: kPrimaryColor,),
+          hintText: "Your email",
+          border: InputBorder.none
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: kPrimaryLightColor,
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          icon: Icon(Icons.person, color: kPrimaryColor,),
+          hintText: "Your email",
+          border: InputBorder.none
+        ),
       ),
     );
   }
